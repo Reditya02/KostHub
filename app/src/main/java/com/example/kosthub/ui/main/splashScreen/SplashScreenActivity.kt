6ff2469412b8fcc2166e06272cab0c20389890a6
pranaditakusuma <komangpranadita@gmail.com>
2023-01-25 01:02:21 +0800

    add: splash screen

---
+++ app/src/main/java/com/example/kosthub/ui/main/splashScreen/SplashScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kosthub.ui.main.splashScreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.kosthub.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
