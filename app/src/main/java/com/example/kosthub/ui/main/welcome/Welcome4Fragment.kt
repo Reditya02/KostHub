7bb5c9760f8764359a0412236ff24b0e746bb718
pranaditakusuma <komangpranadita@gmail.com>
2023-01-25 17:04:34 +0800

    add: onboardings/welcome screens

--- app/src/main/java/com/example/kosthub/ui/main/welcome/Welcome4Fragment.kt
+++ app/src/main/java/com/example/kosthub/ui/main/welcome/Welcome4Fragment.kt
@@ -7,13 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
-import com.example.kosthub.databinding.FragmentWelcome3Binding
+import com.example.kosthub.R
+import com.example.kosthub.databinding.FragmentWelcome4Binding
 import com.example.kosthub.databinding.FragmentWelcomeBinding
-import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint
-class WelcomeFragment : Fragment() {
-    private var _binding: FragmentWelcome3Binding? = null
+
+class Welcome4Fragment : Fragment() {
+    private var _binding: FragmentWelcome4Binding? = null
     private val binding get() = _binding!!
 
     private val viewModel: WelcomeViewModel by viewModels()
@@ -22,7 +22,7 @@ class WelcomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentWelcome3Binding.inflate(inflater, container, false)
+        _binding = FragmentWelcome4Binding.inflate(inflater, container, false)
         return binding.root
     }
 
@@ -31,14 +31,9 @@ class WelcomeFragment : Fragment() {
 
         binding.apply {
 
-            btnLewati.setOnClickListener {
-                val toWelcomeFragment2 = WelcomeFragmentDirections.actionWelcomeFragmentToWelcomeFragment2()
-                findNavController().navigate(toWelcomeFragment2)
-            }
-
-            btnNext.setOnClickListener{
-                val toWelcomeFragment4 = WelcomeFragmentDirections.actionWelcomeFragmentToWelcomeFragment2()
-                findNavController().navigate(toWelcomeFragment4)
+            btnMulai.setOnClickListener{
+                val _toPilihRoleFragment = Welcome4FragmentDirections.actionWelcomeFragment4ToPilihRoleFragment()
+                findNavController().navigate(_toPilihRoleFragment)
             }
 
         }
