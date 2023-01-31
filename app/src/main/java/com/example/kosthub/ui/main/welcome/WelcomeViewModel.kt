61ae7ea5b2eadd4902b7ca577260cf4bb82f8575
Reditya02 <putureditya@gmail.com>
2023-01-31 09:16:52 +0800

    Update: Bug Fixes on WelcomeViewModel.kt

--- app/src/main/java/com/example/kosthub/ui/main/welcome/WelcomeViewModel.kt
+++ app/src/main/java/com/example/kosthub/ui/main/welcome/WelcomeViewModel.kt
@@ -1,13 +1,12 @@
 package com.example.kosthub.ui.main.welcome
 
 import androidx.lifecycle.ViewModel
-import com.example.kosthub.data.Repository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class WelcomeViewModel @Inject constructor(
-    private val repository: Repository
+//    private val repository: Repository
 ) : ViewModel() {
-    fun print() = repository.test()
+//    fun print() = repository.test()
 }
