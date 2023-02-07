ef12ffe9daeca99621d8d86b676e019feafff360
pranaditakusuma <komangpranadita@gmail.com>
2023-02-07 21:18:47 +0800

    update : splash screen

--- app/src/main/java/com/example/kosthub/ui/main/splashScreen/SplashScreenActivity.kt
+++ app/src/main/java/com/example/kosthub/ui/main/splashScreen/SplashScreenActivity.kt
@@ -1,12 +1,27 @@
 package com.example.kosthub.ui.main.splashScreen
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
 import com.example.kosthub.R
+import com.example.kosthub.application.MainActivity
+import com.example.kosthub.ui.main.welcome.WelcomeFragment
+
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        supportActionBar?.hide()
+
+
+//        Handler().postDelayed(Runnable {
+//            val intent = Intent(this@SplashScreenActivity, WelcomeFragment::class.java)
+//            startActivity(intent)
+//            finish()
+//        }, 3000)
+
     }
 }
